9af3a4c1b0fd108b40b107c407035cbd26772c15
Bruna Carmo <47986115+Idbruh@users.noreply.github.com>
2020-03-25 18:28:30 -0300

    Update hangedman.py
    structure of HangedMan game changed once again

--- hangedman.py
+++ hangedman.py
@@ -19,17 +19,25 @@ class HangManGame:
                     |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|   (_)
                                         __/ |                      
                                        |___/          
-                                                    
   _______
  |/      |                                             
  |      (_)
  |      \|/
  |       |
- |      / \ »» Rules:
- |
+ |      / \ »» Rules: 
+ |                »ENTER YOUR NAME TO START THE GAME
+ |                »A WORD WILL BE CHOSEN RANDOMLY. YOU'LL BE ASKED TO ENTER A LETTER
+ |                »YOU OWN 5 CHANCES TO GUESS WRONG ♡ ♡ ♡ ♡ ♡
 _|___                      
 ═══════════════════════════════════════════════════════════════════════════════════════════════════════════════════════
 ''')
+
+    def game_logic(self):
+        pass
+        
+
+
+
 
 class Player:
     def __init__(self):
@@ -42,12 +50,16 @@ class Player:
         p_name = str(input("Please enter the player's name:\n» "))
         self.__p_name = p_name
 
+
+#import Player
 class ChosenLetter:
 
     def __init__(self):
+        self.__player = Player()
         self.__chosen_letter = ''
         self.__wrong_guessed_letter = []
         self.__correct_guessed_letter = []
+        self.__p_lives = 5
 
 
     def get_wrong_players_guess(self):
@@ -63,12 +75,19 @@ class ChosenLetter:
         self.__correct_guessed_letter.append(self.__chosen_letter)
         return self.__correct_guessed_letter
 
-    
+
     def get_chosen_letter(self):
         return self.__chosen_letter
     def set_chosen_letter(self):
-        letter = input(f"{self.__p_name}, please, choose a random letter:\n» ")
+        letter = input(f"{self.__player.get_players_name()}, please, choose a random letter:\n» ")
         return letter
+
+    def get_players_lives(self):
+        return self.__p_lives
+    def lose_a_life(self):
+        self.__p_lives -= 1
+        print(f"{self.__player.get_players_name()}, you've lost a life!")
+
 
 
 import random
@@ -80,6 +99,8 @@ class FruitList:
     def chose_random_word_from_list(self):
         fruit = self.__fruits
         return random.choice(fruit)
+
+
 
 
 
